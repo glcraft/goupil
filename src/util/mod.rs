48d75d5c79c4b0ef6cd5f61ee6da3c50280e331c
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-12-05 18:12:01 +0100

    improve urlencode

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,38 +1,3 @@
 pub mod base64;
 pub mod sha;
-
-pub fn urlencode(input: &str) -> String {
-    let len = input
-        .chars()
-        .map(|c| {
-            if c.is_ascii_alphanumeric() {
-                1
-            } else {
-                c.len_utf8() * 3
-            }
-        })
-        .sum();
-    let mut output = String::with_capacity(len);
-    // let mut urlencoded = String::new();
-    for c in input.chars() {
-        if c.is_ascii_alphanumeric() {
-            output.push(c);
-        } else {
-            let (bytes, len) = {
-                let mut bytes = [0; 4];
-                let len = { c.encode_utf8(&mut bytes).len() };
-                (bytes, len)
-            };
-            // urlencoded.clear();
-            // urlencoded.reserve(s.len() * 3);
-            bytes.into_iter().take(len).for_each(|c| {
-                output.push('%');
-                unsafe {
-                    output.push(char::from_digit((c >> 4) as u32, 16).unwrap_unchecked());
-                    output.push(char::from_digit((c & 0xf) as u32, 16).unwrap_unchecked());
-                }
-            });
-        }
-    }
-    output
-}
+pub mod urlencode;
---
+++ src/util/urlencode.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+pub fn len(input: &str) -> usize {
+    input
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphanumeric() {
+                1
+            } else {
+                c.len_utf8() * 3
+            }
+        })
+        .sum()
+}
+pub fn encode_inplace(input: &str, output: &mut String) {
+    let output_len = len(input);
+    output.reserve(output_len);
+    for c in input.chars() {
+        if c.is_ascii_alphanumeric() {
+            output.push(c);
+            continue;
+        }
+        let mut bytes = [0; 4];
+        let len = c.encode_utf8(&mut bytes).len();
+        bytes.into_iter().take(len).for_each(|c| {
+            output.push('%');
+            unsafe {
+                output.push(char::from_digit((c >> 4) as u32, 16).unwrap_unchecked());
+                output.push(char::from_digit((c & 0xf) as u32, 16).unwrap_unchecked());
+            }
+        });
+    }
+}
+pub fn encode(input: &str) -> String {
+    let mut output = String::new();
+    encode_inplace(input, &mut output);
+    output
+}
+
+pub trait IntoUrlEncoded {
+    fn into_url_encoded(&self) -> String;
+}
+
+impl<S1: AsRef<str>, S2: AsRef<str>> IntoUrlEncoded for HashMap<S1, S2> {
+    fn into_url_encoded(&self) -> String {
+        let output_len: usize = self.keys().map(|s| s.as_ref().len()).sum::<usize>()
+            + self.values().map(|v| self::len(v.as_ref())).sum::<usize>()
+            + (self.len() + 1);
+        let mut output = String::with_capacity(output_len);
+        for (k, v) in self.iter() {
+            output.push_str(k.as_ref());
+            output.push('=');
+            self::encode_inplace(v.as_ref(), &mut output);
+        }
+        output
+    }
+}
