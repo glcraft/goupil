3c51087c3d8af27e0f09e839c1e65aee9585bd7e
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-12-07 02:46:51 +0100

    add utility helpers

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,34 @@
 pub mod base64;
 pub mod sha;
 pub mod urlencode;
+
+#[macro_export]
+macro_rules! hashmap {
+    ( $(($key:expr, $value:expr $(,)?)),+ $(,)? ) => {{
+        let mut hm = std::collections::HashMap::new();
+        $(
+        hm.insert($key, $value);
+        )+
+        hm
+    }}
+}
+
+#[inline]
+#[cold]
+fn cold() {}
+
+#[inline]
+pub fn likely(b: bool) -> bool {
+    if !b {
+        cold()
+    }
+    b
+}
+
+#[inline]
+pub fn unlikely(b: bool) -> bool {
+    if b {
+        cold()
+    }
+    b
+}
