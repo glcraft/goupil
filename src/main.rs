3e7fe90ab3129b4efa62da60221c1c55b4cff52b
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-12-12 00:24:28 +0100

    add log and simple_logger

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ mod terminal;
 mod util;
 
 fn main() {
+    simple_logger::SimpleLogger::new()
+        .env()
+        .init()
+        .expect("unable to init simple_logger");
     let api_config = secrets::ApiConfig::load();
     gmail::get_credentials(&api_config);
 }
